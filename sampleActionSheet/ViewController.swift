7a21fea31eddd526b9591dc37284c69fd5e2a5b1
ei17ringo <ei17ringo@gmail.com>
2016-01-30 08:44:47 +0800

    commit init

--- sampleActionSheet/ViewController.swift
+++ sampleActionSheet/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,59 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapBtn(sender: UIButton) {
+        
+        
+        //アクションシートを作る
+        var alertController = UIAlertController(title: "行くアクティビティ", message: "どれにする？", preferredStyle: UIAlertControllerStyle.ActionSheet)
+        
+       
+        // アクション１ボタンを追加
+        alertController.addAction(UIAlertAction(
+            title: "ヨガ",
+            style: .Default,
+            handler: { action in self.myAction("ヨガ") } ))
+        // アクション２ボタンを追加
+        alertController.addAction(UIAlertAction(
+            title: "バスケ",
+            style: .Default,
+            handler: { action in self.myAction("バスケ") } ))
+        
+        // 削除ボタンを追加
+        alertController.addAction(UIAlertAction(
+            title: "ムエタイ",
+            style: .Destructive,
+            handler: { action in self.myDelete() } ))
+        // キャンセルボタンを追加
+        alertController.addAction(UIAlertAction(
+            title: "今日はやめとく",
+            style: .Cancel,
+            handler: { action in self.myCancel() } ))
+        // アラートを表示する
+        presentViewController(alertController, animated: true, completion: nil)
+        
+    }
+    
+    // アクションが選択されたとき呼ばれるメソッド
+    func myAction(var activitiy:String!) {
+        print("アクション")
+        myLabel.text = "\(activitiy)にいこう！"
+    }
+    // 削除が選択されたとき呼ばれるメソッド
+    func myDelete() {
+        print("削除")
+        myLabel.text = "ムエタイ頑張ろ！"
+    }
+    // キャンセルが選択されたとき呼ばれるメソッド
+    func myCancel() {
+        print("キャンセル")
+        myLabel.text = "明日は行こう！"
+    }
 
+
+    
 }
+
+
+
 
